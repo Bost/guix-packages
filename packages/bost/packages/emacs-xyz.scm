873c464ff5dcb95c0cee607459d22bed243883a0
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-11-03 22:20:11 +0100

    gnu: emacs-evil-anzu: Update to 0.03-0.d1e98ee.
    The github url redirects from syohex/emacs-evil-anzu to emacsorphanage/evil-anzu
    
    * gnu/packages/emacs-xyz.scm (emacs-evil-anzu): Update to 0.03-0.d1e98ee.

--- packages/bost/packages/emacs-xyz.scm
+++ packages/bost/packages/emacs-xyz.scm
@@ -4281,6 +4281,30 @@ Debug server.")
       (description "")
       (license license:gpl3+))))
 
+(define-public emacs-evil-anzu
+  (let ((commit "d1e98ee6976437164627542909a25c6946497899")
+        (revision "0"))
+    (package
+      (name "emacs-evil-anzu")
+      (version (git-version "0.03" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/emacsorphanage/evil-anzu")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1i8f360lq5a32knkzbwdw10ql9cxsmgfd4iiwnr7vcwacm34zq88"))))
+      (build-system emacs-build-system)
+      (propagated-inputs
+       (list emacs-evil emacs-anzu))
+      (home-page "https://github.com/emacsorphanage/evil-anzu")
+      (synopsis "Anzu for evil-mode")
+      (description "@code{anzu} provides a minor mode that displays the current
+match and total match information in the mode-line in various search modes.")
+      (license license:gpl3+))))
+
 #|
 (load "/home/bost/dev/dotfiles/guix/home/utils.scm")
 (load "/home/bost/dev/guix-packages/packages/bost/packages/emacs-xyz.scm")
